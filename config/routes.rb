5355acfd0fb5e94fa1d66a6e63ce95136641f554
Dalton <dalthon@dlt.local>
2014-12-30 11:01:56 -0200

    Fixed route methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Phaseout::Engine.routes.draw do
   root to: 'phaseout#index'
 
-  get '/actions',            to: 'phaseout#actions',     as: :seo_action_list
-  get '/action/:key/keys',   to: 'phaseout#action_keys', as: :seo_action_keys
-  get '/fields/:key/update', to: 'phaseout#update',      as: :seo_fields_update
-  get '/fields/:key/delete', to: 'phaseout#delete',      as: :seo_fields_delete
+  get    '/actions',            to: 'phaseout#actions',     as: :seo_action_list
+  get    '/action/:key/keys',   to: 'phaseout#action_keys', as: :seo_action_keys
+  put    '/fields/:key/update', to: 'phaseout#update',      as: :seo_fields_update
+  delete '/fields/:key/delete', to: 'phaseout#delete',      as: :seo_fields_delete
 end
